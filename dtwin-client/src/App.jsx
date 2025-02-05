93214c59b5d051ae9fc9b41a39bf9f5191d892fd
Kishore <ki225ku@gmail.com>
2025-02-05 16:32:37 +0530

    route added

--- dtwin-client/src/App.jsx
+++ dtwin-client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/ui/Home";
 import SignIn from "./components/ui/SignIn";
 import SignUp from "./components/ui/SignUp";
+import { path } from 'path';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/search" element={<SearchBox />}></Route>
       </Routes>
     </Router>
   );
